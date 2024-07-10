78905c87742507dcc58e40df9b095e64ac78c5b9
joshuaavalo <avalojoshua83@gmail.com>
2024-07-10 20:02:32 -0300

    modificacion de conexion

--- server/src/db.js
+++ server/src/db.js
@@ -4,15 +4,23 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/campeones`,
+   `${DB_USER}`,
+   
    {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+      dialectOptions: {
+         ssl: {
+           require: true,
+           rejectUnauthorized: false, //requiere esto para funcionar.
+         },
+       },
    }
 );
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
